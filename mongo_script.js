a22e4166a1247dcedc6a4e2f4f7c01bd5f56281c
kezoj <jozekzienowicz@gmail.com>
2020-11-03 20:01:11 +0100

    account changes

--- mongo_script.js
+++ mongo_script.js
@@ -7,16 +7,16 @@ db.Users.drop()
 db.Transactions.drop()
 
 db.Accounts.insertMany([
-	{ amount: 5000 },
-	{ amount: 1000 },
+	{ amount: 5000, account_number: 1  },
+	{ amount: 1000, account_number: 2  },
 ]);
 
-var account_1 = db.Accounts.findOne({ amount: 5000 });
-var account_2 = db.Accounts.findOne({ amount: 1000 });
+var account_1 = db.Accounts.findOne({ account_number: 1 });
+var account_2 = db.Accounts.findOne({ account_number: 2 });
 
 db.Users.insertMany([
-	{ name: 'user_1', password: 'password', account_id: account_1._id },
-	{ name: 'user_2', password: 'password', account_id: account_2._id },
+	{ name: 'user_1', password: 'password', account_id: account_1._id},
+	{ name: 'user_2', password: 'password', account_id: account_2._id},
 ])
 
 var user_1 = db.Accounts.findOne({ amount: 5000 });
