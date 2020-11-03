9c0335833e4e979e357d4f7a894c21f70b9ad66c
kezoj <jozekzienowicz@gmail.com>
2020-11-03 19:53:05 +0100

    mongo script

---
+++ mongo_script.js
@@ -0,0 +1,27 @@
+// JavaScript source code
+conn = new Mongo();
+db = conn.getDB("Bank");
+
+db.Accounts.drop()
+db.Users.drop()
+db.Transactions.drop()
+
+db.Accounts.insertMany([
+	{ amount: 5000 },
+	{ amount: 1000 },
+]);
+
+var account_1 = db.Accounts.findOne({ amount: 5000 });
+var account_2 = db.Accounts.findOne({ amount: 1000 });
+
+db.Users.insertMany([
+	{ name: 'user_1', password: 'password', account_id: account_1._id },
+	{ name: 'user_2', password: 'password', account_id: account_2._id },
+])
+
+var user_1 = db.Accounts.findOne({ amount: 5000 });
+var user_2 = db.Accounts.findOne({ amount: 1000 });
+
+db.Transactions.insertOne(
+	{ sender_account_id: user_1._id, reciver_account_id: user_2._id, amount: 500 }
+)
